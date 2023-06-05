76c3c60f65c4205e6057c1a6e8826410d7469fab
mayi0815 <mayinuer0815@gmail.com>
2023-06-06 00:18:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Chatbot NodeJS
+
+Chatbot with NodeJS and Express, integrating DialogFlow API. 
+
+# To use
+To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) which comes with [npm](http://npmjs.com) installed on your computer. 
+
+You can run the server from terminal and view the bot opening `http://localhost:8199` on your browser:
+~~~~
+# Clone this repository
+git clone https://github.com/mayi0815/mychatbot-nodejs
+
+# Run the app
+npm start
+
+# Open in your browser: http://localhost:8199
+~~~~
+
+Also you can build the app for platform you wish from run scripts (package.json).
---
+++ index.js
@@ -0,0 +1,56 @@
+// imports
+const apiai = require('apiai')('9a638e79385a4377b4d307b1cd3b1940');
+const express = require('express');
+const app = express();
+
+
+// config
+app.set('port', process.env.PORT || 8199);
+
+
+// static files
+app.use(express.static(__dirname + '/views')); // html
+app.use(express.static(__dirname + '/public')); // js, css, images
+
+
+// routing
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+});
+
+
+// initialize server
+const server = app.listen(app.get('port'), () => {
+    console.log('Server listening on port http://localhost:8199',app.get('port'));
+});
+var io = require('socket.io')(server);
+
+
+// io connection
+io.on('connection', function(socket) {
+    socket.on('chat message', (text) => {
+        var sessionID = new Date().getTime();
+        // console.log('chat message', text, sessionID);
+
+        // get reply from DialogFlow
+        //@TODO : REMPLACE WITH RETURNING MESSAGE OF CHATGPT
+        let apiaiReq = apiai.textRequest(text, {
+            sessionId: sessionID //APIAI_SESSION_ID
+        });
+
+        apiaiReq.on('response', (response) => {
+            let aiText = response.result.fulfillment.speech;
+            socket.emit('bot reply', aiText); // send result back to browser
+            console.log('bot reply');
+        });
+
+        apiaiReq.on('error', (error) => {
+            console.log(error);
+        });
+
+        apiaiReq.end()
+    });
+});
+
+//openai connection 
+//token : XXXXX
